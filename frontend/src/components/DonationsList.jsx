2023be9be6cde4d4dcb23d80310926660e7f76d2
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-03-09 23:17:29 +0530

    add a popup component for confirmation

--- frontend/src/components/DonationsList.jsx
+++ frontend/src/components/DonationsList.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/list.css";
+import Confirmation from "./Confirmation";
 
 export default function DonationsList() {
     const [recipients, setRecipients] = useState([]);
@@ -38,14 +39,15 @@ export default function DonationsList() {
 
     return (
         <div>
-            <div>Nearby Requests</div>
+            <h1>Nearby Requests</h1>
             <div className="mainContainer">
                 {recipients.map((element) => (
                     <Donation element={element} />
                 ))}
             </div>
             <div className="bottoms-buttons">
-                <button className="btns-3"
+                <button
+                    className="btns-3"
                     onClick={() => {
                         navigate("/donationRequest");
                     }}
@@ -58,36 +60,45 @@ export default function DonationsList() {
 }
 
 function Donation({ element }) {
-    const navigate = useNavigate();
+    const [isPopupOpen, setPopupOpen] = useState(false);
 
-    function handleClick(){
-        console.log();
-    }    
+    function handleClick() {
+        setPopupOpen(true);
+    }
 
     return (
-        <div className="product-container" >
+        <div className="product-container">
             <div className="product-image-container">
-            <img className="product-image" src="/assets/images/mapapi.png" style={{ width: "100%" }} alt="" />
-            </div>            
+                <img
+                    className="product-image"
+                    src="/assets/images/mapapi.png"
+                    style={{ width: "100%" }}
+                    alt=""
+                />
+            </div>
             <div className="product-description-container">
                 <div className="description">
                     Description: {element.description}
                 </div>
-                <div className="serves">
-                    To Serve: {element.toServe}
-                </div>
+                <div className="serves">To Serve: {element.toServe}</div>
                 <div className="date">
                     Date: {new Date(element.date).toLocaleDateString("en-GB")}
                 </div>
                 <div className="addresspro">
                     Address: {element.addressTo.street}
                 </div>
-                <div className="citypro">
-                    City: {element.addressTo.city}
-                </div>
+                <div className="citypro">City: {element.addressTo.city}</div>
                 <div className="container-button">
-                    <button onClick={handleClick} className="button">Donate</button>
+                    <button onClick={handleClick} className="button">
+                        Donate
+                    </button>
                 </div>
+                {isPopupOpen && (
+                    <Confirmation
+                        element={element}
+                        onClose={() => setPopupOpen(false)}
+                    />
+                )}
             </div>
         </div>
     );
