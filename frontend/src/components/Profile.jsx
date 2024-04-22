3dc8b8c991a8d6ae880cb9d5646d2cf0bf7e6b7f
Krish <krishagarwal1673@gmail.com>
2024-04-22 18:59:05 +0530

    Create  profile page

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -3,9 +3,36 @@ import { Navigate } from "react-router-dom";
 import "../styles/Profile.css";
 
 export default function Component(){
+
+    const handleLogout = () =>{
+        fetch("http://localhost:3000/api/auth/logout",{
+            method:"POST",
+            headers: {
+                authorization:
+                    "Bearer " + localStorage.getItem("token"),
+            },
+            body: JSON.stringify({})
+        })
+        .then((response)=>response.json())
+        .then((data)=>{
+            if (data.errorMessage) {
+                alert(data.errorMessage);
+            } else {
+                Navigate("/Annamitra");
+                window.location.reload();
+            }
+        });
+    }
+    
+
     return (
         <div>
-            hi
+            <button
+                    className="btns-main"
+                    onClick= {handleLogout}
+                    >
+                        Logout
+                    </button>
         </div>
     )
 }
