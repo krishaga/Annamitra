e64fe319fd7cd0a106f4a29b94c45bd06bedd7a0
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-05-30 23:35:58 +0530

    fix: pfp upload issue
    Co-authored-by: Krish Agarwal <krishaga@users.noreply.github.com>

--- backend/middlewares/dpUpload.js
+++ backend/middlewares/dpUpload.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../uploads/profile-pictures')); // Use path.join to ensure platform-independent file paths
+        cb(null, path.join(__dirname, '../uploads/profile-pictures'));
     },
     filename: function (req, file, cb) {
-        // Use a unique filename, such as a timestamp or random string, to avoid naming conflicts
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, 'profile-picture-' + uniqueSuffix + file.originalname + path.extname(file.originalname));
+        cb(null, 'profile-picture-' + uniqueSuffix + path.extname(file.originalname));
     }
 });
+
 
 const upload = multer({ storage: storage });
 
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models/user');
-const upload = require('../middlewares/dpUpload')
+const upload = require('../middlewares/dpUpload');
+const authenticateJwt = require('../middlewares/authentication');
 
 // Route for uploading profile pictures
-router.post('/upload-profile-picture', upload.single('file'), async (req, res) => {
+router.post('/upload-profile-picture', authenticateJwt, upload.single('file'), async (req, res) => {
     try {
-        // Save file path in MongoDB
         const profile = await User.findOneAndUpdate(
-            { username: req.user.username }, // Assuming you have a way to identify the user
-            { profilePicture: req.file.path }, // Save file path
+            { username: req.user.username },
+            { profilePicture: `/uploads/profile-pictures/${req.file.filename}` }, // Save relative file path
             { new: true, upsert: true }
         );
         console.log('Profile picture uploaded:', profile.profilePicture);
-        res.json({ success: true });
+        res.json({ success: true, profilePicture: profile.profilePicture });
     } catch (error) {
         console.error('Error uploading profile picture:', error);
         res.status(500).json({ success: false, message: 'Failed to upload profile picture' });
